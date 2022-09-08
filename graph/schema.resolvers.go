9ab80152c52d07784dad8d60ab9d89d4b9770e65
Jamesson Faria <jamessonjr@gmail.com>
2022-09-08 20:00:39 -0300

    Implemented queries

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -28,17 +28,17 @@ func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewCha
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	return r.Resolver.Categories
+	return r.Resolver.Categories, nil
 }
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	return r.Resolver.Courses, nil
 }
 
 // Chapters is the resolver for the chapters field.
 func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented: Chapters - chapters"))
+	return r.Resolver.Chapters, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
